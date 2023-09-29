bc977c2a2539f2c05755028fb373f6885916e499
avorio-dev <avorio.dev@gmail.com>
2023-09-29 17:40:40 +0200

    Added Themes

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,19 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	'sap/ui/model/json/JSONModel'
+
+], function (Controller, JSONModel) {
 	"use strict";
 
-	/* 
-		Hide Loading Bar
-	*/
-	function hideLoadingBar(oContext) {
+	/* Hide Loading Bar */
+	function _hideLoadingBar(oContext) {
 		var oLoadingBar = oContext.getView().byId("loadingBar");
 		oLoadingBar.setVisible(false);
 	}
 
 
-	/* 
-		Show Loading Bar 
-	*/
-	function showLoadingBar(oContext) {
+	/* Show Loading Bar */
+	function _showLoadingBar(oContext) {
 		var oLoadingBar = oContext.getView().byId("loadingBar");
 
 		oLoadingBar.setVisible(true);
@@ -29,10 +27,12 @@ sap.ui.define([
 		If you want to hide some component, for example when loading bar is filling up,
 		add here your component
 	*/
-	function hideComponent(oContext) {
-		var oStartButton = oContext.getView().byId("startBtn");
+	function _hideComponent(oContext) {
+		var oStartButton = oContext.getView().byId("startBtn"),
+			oThemeSelect = oContext.getView().byId("themeSelect");
 
 		oStartButton.setVisible(false);
+		oThemeSelect.setVisible(false);
 	}
 
 
@@ -40,47 +40,108 @@ sap.ui.define([
 		If you want to show some components, for example when loading bar is full,
 		add here your component 
 	*/
-	function showComponent(oContext) {
+	function _showComponent(oContext) {
 		var oLoadingBar = oContext.getView().byId("loadingBar");
 
 		if (oLoadingBar.getPercentValue() === 100) {
 			// set visible = true to your component here 
-			
-			var oStartButton = oContext.getView().byId("startBtn");
+
+			var oStartButton = oContext.getView().byId("startBtn"),
+				oThemeSelect = oContext.getView().byId("themeSelect");
 
 			oStartButton.setVisible(true);
+			oThemeSelect.setVisible(true);
 		}
 	}
 
 
+	/* Theme Switch Function */
+	function _setTheme(oContext) {
+		var oThemeSelect = oContext.getView().byId("themeSelect");
+		var selectedKeyTheme = oThemeSelect.getSelectedKey();
+
+		sap.ui.getCore().applyTheme(selectedKeyTheme);
+	}
+
+
 	return Controller.extend("UI5IceCreamMachine.controller.App", {
+
+		/* Initialize all components on first call of the page */
 		onInit: function () {
 
-			// hideLoadingBar(this);
-			hideComponent(this);
+			// Set THEME Selector
+			var oDataThemes = {
+				"SelectedTheme": "sap_horizon",
+				"ThemesCollection": [
+					{
+						"ThemeId": "sap_horizon",
+						"Name": "Morning Horizon"
+					},
+					{
+						"ThemeId": "sap_horizon_dark",
+						"Name": "Evening Horizon"
+					},
+					{
+						"ThemeId": "sap_belize",
+						"Name": "Belize"
+					},
+					{
+						"ThemeId": "sap_belize_plus",
+						"Name": "Belize Plus"
+					},
+					{
+						"ThemeId": "sap_bluecrystal",
+						"Name": "SAP Blue Crystal"
+					},
+					{
+						"ThemeId": "sap_fiori_3",
+						"Name": "Quartz Light"
+					},
+					{
+						"ThemeId": "sap_fiori_3_dark",
+						"Name": "Quartz Dark"
+					}
+				]
+			};
+			var oModelThemes = new JSONModel(oDataThemes);
+			this.getView().setModel(oModelThemes, "modelThemes");
+			_setTheme(this);
 
-			setTimeout(showLoadingBar, 1000, this);
+
+			// Set LOADING Bar and component visibility
+			_hideLoadingBar(this);
+			_hideComponent(this);
+			setTimeout(_showLoadingBar, 1000, this);
 
 			var maxProgress = 100,
 				updateInterval = 15,
 				currentValue = 0;
 
-			function loadingBar(oContext) {
+			function _loadingBar(oContext) {
 				var oLoadingBar = oContext.getView().byId("loadingBar");
 
 				if (currentValue <= maxProgress) {
 					oLoadingBar.setPercentValue(+currentValue);
 					oLoadingBar.setDisplayValue(currentValue + "%");
-					setTimeout(loadingBar, updateInterval, oContext);
+					setTimeout(_loadingBar, updateInterval, oContext);
 
 					currentValue += 1;
 				} else {
-					setTimeout(hideLoadingBar, 200, oContext);
-					setTimeout(showComponent, 250, oContext);
+					setTimeout(_hideLoadingBar, 200, oContext);
+					setTimeout(_showComponent, 250, oContext);
 				}
 			}
 
-			setTimeout(loadingBar, 1500, this);
+			setTimeout(_loadingBar, 1500, this);
+		},
+
+		/* Function that will be called on Start Button Press */
+		onStartBtn: function () {
+			window.open("https://shorturl.at/tILT5", "_blank");
+		},
+
+		onSelectTheme: function () {
+			_setTheme(this);
 		}
 
 	});
