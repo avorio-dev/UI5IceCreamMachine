962c6dc26239d9e6231c848fb45c5dd0aacb262c
avorio-dev <avorio.dev@gmail.com>
2023-10-03 14:25:19 +0200

    Add Dashboard View

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    'sap/ui/model/json/JSONModel'
+    'sap/ui/model/json/JSONModel',
+    "sap/ui/model/xml/XMLModel"
 
-], function (Controller, JSONModel) {
+], function (Controller, JSONModel, XMLModel) {
     "use strict";
 
     return Controller.extend("UI5IceCreamMachine.controller.Dashboard", {
@@ -10,7 +11,49 @@ sap.ui.define([
         /* Initialize all components on first call of the page */
         onInit: function () {
 
+            //var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/03/model/data") + "/News.json";
+            //var oModel = new JSONModel(sDataPath);
 
+            /* ops
+            var sRssLink = "https://www.ansa.it/sito/ansait_rss.xml";
+            var oXmlModel = new XMLModel();
+
+            oXmlModel.loadData(sRssLink);
+            this.getView().setModel(oXmlModel, "news");
+            */
+
+            var oModel = this.getOwnerComponent().getModel("feeds");
+            console.log(oModel.getData());
+
+        },
+
+        getProgress: function (aNodes) {
+            if (!aNodes || aNodes.length === 0) {
+                return 0;
+            }
+            var iSum = 0;
+            for (var i = 0; i < aNodes.length; i++) {
+                iSum += aNodes[i].state === "Positive";
+            }
+            var fPercent = (iSum / aNodes.length) * 100;
+            return fPercent.toFixed(0);
+        },
+
+        getEntityCount: function (entities) {
+            return entities && entities.length || 0;
+        },
+
+        formatNumber: function (value) {
+            var oFloatFormatter = NumberFormat.getFloatInstance({
+                style: "short",
+                decimals: 2
+            });
+            return oFloatFormatter.format(value);
+        },
+
+        formatJSONDate: function (date) {
+            var oDate = new Date(Date.parse(date));
+            return oDate.toLocaleDateString();
         }
     });
 });
