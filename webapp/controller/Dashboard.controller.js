a5b39afc9eefddac250ece660c712e705e8847cc
avorio-dev <avorio.dev@gmail.com>
2023-10-04 09:57:01 +0200

    Added Slide Tiles

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -6,22 +6,76 @@ sap.ui.define([
 ], function (Controller, JSONModel, XMLModel) {
     "use strict";
 
+    /* Fill Tile with data loaded from XML Model */
+    function _initXMLTile(oContext) {
+
+        //var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/03/model/data") + "/News.json";
+        //var oModel = new JSONModel(sDataPath);
+
+        /*
+        var sRssLink = "https://www.ansa.it/sito/ansait_rss.xml";
+        var oXmlModel = new XMLModel();
+
+        oXmlModel.loadData(sRssLink);
+        this.getView().setModel(oXmlModel, "feeds");
+        */
+
+        var oSlideTileXml = oContext.getView().byId("slideTileXml");
+        var oFeedsModel = oContext.getOwnerComponent().getModel("feeds");
+
+        var aItems = oFeedsModel.getData().getElementsByTagName("item");
+        Array.from(aItems).forEach(function (oItem) {
+
+            var oGenericTile = new sap.m.GenericTile({
+                header: oItem.getElementsByTagName("title")[0].textContent,
+                subheader: oItem.getElementsByTagName("description")[0].textContent,
+                
+                press: function () {
+                    //window.open("https://shorturl.at/tILT5", "_blank");
+                    var link = oItem.getElementsByTagName("link")[0].textContent;
+                    window.open(link, "_blank");
+                }
+            });
+
+            var oTileContent = new sap.m.TileContent({
+                footer: oItem.getElementsByTagName("pubDate")[0].textContent,
+            });
+
+            oGenericTile.addTileContent(oTileContent);
+            oSlideTileXml.addTile(oGenericTile);
+        });
+    }
+
+    /* Fill Tile with data loaded from XML Model and Converted in JSON Model */
+    function _initJSONTile(oContext) {
+        var oSlideTileXmlToJson = oContext.getView().byId("slideTileXmlToJson");
+        var oFeedsModel = oContext.getOwnerComponent().getModel("feeds");
+
+        var aTileItems = [];
+        var aItems = oFeedsModel.getData().getElementsByTagName("item");
+        Array.from(aItems).forEach(function (oItem) {
+            var oTileItem = {
+                title: oItem.getElementsByTagName("title")[0].textContent,
+                description: oItem.getElementsByTagName("description")[0].textContent,
+                pubDate: oItem.getElementsByTagName("pubDate")[0].textContent,
+                link: oItem.getElementsByTagName("link")[0].textContent
+            };
+
+            aTileItems.push(oTileItem);
+        });
+
+        var oTileModel = new sap.ui.model.json.JSONModel();
+        oTileModel.setData({ tiles: aTileItems });
+        oSlideTileXmlToJson.setModel(oTileModel, "feedsXmlToJson");
+
+    }
+
     return Controller.extend("UI5IceCreamMachine.controller.Dashboard", {
 
         /* Initialize all components on first call of the page */
         onInit: function () {
-
-            //var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/03/model/data") + "/News.json";
-            //var oModel = new JSONModel(sDataPath);
-
-
-            /*
-            var sRssLink = "https://www.ansa.it/sito/ansait_rss.xml";
-            var oXmlModel = new XMLModel();
-
-            oXmlModel.loadData(sRssLink);
-            this.getView().setModel(oXmlModel, "feeds");
-            */
+            _initXMLTile(this);
+            _initJSONTile(this);
 
             /* 
             var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/03/model/data") + "/News.json";
@@ -29,33 +83,7 @@ sap.ui.define([
             this.getView().setModel(oNewsModel, "news");
             */
 
-            // Assume che il tuo modello sia già caricato con i dati
-            var oSlideTile = this.getView().byId("slideTileId");
-            var oFeedsModel = this.getOwnerComponent().getModel("feeds");
 
-            var aItems = oFeedsModel.getData().getElementsByTagName("item");
-            Array.from(aItems).forEach(function (oItem) {
-
-                var link = oItem.getElementsByTagName("link")[0].textContent;
-
-                var oGenericTile = new sap.m.GenericTile({
-                    header: oItem.getElementsByTagName("title")[0].textContent,
-                    subheader: oItem.getElementsByTagName("description")[0].textContent,
-                    info: oItem.getElementsByTagName("pubDate")[0].textContent,
-
-                    press: function () {
-                        //window.open("https://shorturl.at/tILT5", "_blank");
-                        window.open(link, "_blank");
-                    }
-                });
-
-                var oTileContent = new sap.m.TileContent({
-                    footer: new sap.m.Link({ text: "Leggi di più", href: link })
-                });
-
-                oGenericTile.addTileContent(oTileContent);
-                oSlideTile.addTile(oGenericTile);
-            });
         },
 
         getProgress: function (aNodes) {
@@ -85,6 +113,10 @@ sap.ui.define([
         formatJSONDate: function (date) {
             var oDate = new Date(Date.parse(date));
             return oDate.toLocaleDateString();
+        },
+
+        formatMessage: function () {
+
         }
     });
 });
