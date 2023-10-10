1848574b6b4482f0efe39b83963a67dafd259bb0
avorio-dev <avorio.dev@gmail.com>
2023-10-10 14:28:31 +0200

    Bug Fix

--- webapp/controller/ChartContainer.controller.js
+++ webapp/controller/ChartContainer.controller.js
@@ -1,14 +1,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "../libs/Utils"
+    "../libs/Utils",
+    "sap/base/strings/formatMessage"
 
-], function (Controller, Utils) {
+], function (Controller, Utils, formatMessage) {
     "use strict";
 
     return Controller.extend("UI5IceCreamMachine.controller.ChartContainer", {
 
         onNavBack: function (oEvent) {
             Utils.onNavBack(this);
-        }
+        },
+
+        formatMessage: formatMessage,
     });
 });
